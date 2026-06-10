25327d7fe1dc45eebc625d9d6d24448b49fca3a3
ToujoursPareil8 <jasonaridy@gmail.com>
2026-06-10 10:41:12 +0200

    feat: added apps.py in menuisier/apps/api and fixed wrong application naming

--- menuisierpro/apps/api/apps.py
+++ menuisierpro/apps/api/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-class MenuisierConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.menuisier'
+    name = 'apps.api'
